ef939470e0b7f5a21ede4ef082c2264c194d5bd7
hli155 <hli155@uclive.ac.nz>
2021-03-04 20:03:06 +1300

    test and set data format for sort by ID and sort by date

--- app/middleware/sortById.js
+++ app/middleware/sortById.js
@@ -10,6 +10,20 @@ exports.sortById = function(result){
         }
     }
     return result
-
-
 }
+
+
+/*
+[
+ { id: 17, name: 'Arts' },{ id: 16, name: 'Beliefs' },{ id: 18, name: 'Book Clubs' },{ id: 24, name: 'Career & Business' }
+]
+
+*/
+
+let data = [{id:1,title:"animals",descptions:"for animail warefare",date:"2020-10-28 09:00:00"},
+{id:8,title:"human",descptions:"for animail warefare",date:"2020-10-29 09:00:00"},
+{id:6,title:"animals",descptions:"for animail warefare",date:"2020-10-30 09:00:00"},
+{id:2,title:"animals",descptions:"for animail warefare",date:"2020-10-31 09:00:00"},
+{id:10,title:"animals",descptions:"for animail warefare",date:"2020-10-32 09:00:00"},
+{id:4,title:"animals",descptions:"for animail warefare",date:"2020-10-44 09:00:00"}
+]
